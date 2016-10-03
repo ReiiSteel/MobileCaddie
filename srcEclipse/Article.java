ef9e61cdfc4a85eee926ae4d4f63e5e5333974c7
Johan MARTIN <E145628R@U-INF-J-E114-11.iut-nantes.univ-nantes.prive>
2016-10-03 17:27:22 +0200

    t'es une merde

--- srcEclipse/Article.java
+++ srcEclipse/Article.java
@@ -5,4 +5,33 @@ public class Article {
 	private String nom;
 	private float prixLocationParJour;
 	private int nbStock;
+	
+	public Article(String nom, String reference, String modele, float prixLocationParJour, int nbStock) {
+		this.nom = nom;
+		this.reference = reference;
+		this.marque = modele;
+		this.prixLocationParJour = prixLocationParJour;
+		this.nbStock = nbStock;		
+	}
+
+	public String getReference() {
+		return reference;
+	}
+
+	public String getMarque() {
+		return marque;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public float getPrixLocationParJour() {
+		return prixLocationParJour;
+	}
+
+	public int getNbStock() {
+		return nbStock;
+	}
+	
 }
--- srcEclipse/Magasin.java
+++ srcEclipse/Magasin.java
@@ -1,7 +1,86 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Magasin {
 	private String nom;
 	private ArrayList<Client> listeClients;
 	private ArrayList<Article> articles;
+	
+	public Magasin(String nom) {
+		this.nom = nom;
+		this.listeClients = new ArrayList<Client>();
+		this.articles = new ArrayList<Article>();
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public ArrayList<Client> getListeClients() {
+		return listeClients;
+	}
+
+	public void setListeClients(ArrayList<Client> listeClients) {
+		this.listeClients = listeClients;
+	}
+
+	public ArrayList<Article> getArticles() {
+		return articles;
+	}
+
+	public void setArticles(ArrayList<Article> articles) {
+		this.articles = articles;
+	}
+	
+	public ArrayList<Article> getArticlesLouesByRef(String reference) {
+	    Iterator itr = articles.iterator();
+	    ArrayList<Article> artReturned = new ArrayList<Article>();
+	    while(itr.hasNext()) {
+	    	Article art = (Article) itr.next();
+	    	if(art.getReference() == reference) {
+	    		artReturned.add(art);
+	    	}	    	
+	    }
+		return artReturned;
+	}
+	
+	public ArrayList<Article> getArticlesLouesByMarque(String marque) {
+		Iterator itr = articles.iterator();
+	    ArrayList<Article> artReturned = new ArrayList<Article>();
+	    while(itr.hasNext()) {
+	    	Article art = (Article) itr.next();
+	    	if(art.getMarque() == marque) {
+	    		artReturned.add(art);
+	    	}	    	
+	    }
+		return artReturned;
+	}
+	
+	public ArrayList<Article> getArticlesLouesByNom(String nom) {
+		Iterator itr = articles.iterator();
+	    ArrayList<Article> artReturned = new ArrayList<Article>();
+	    while(itr.hasNext()) {
+	    	Article art = (Article) itr.next();
+	    	if(art.getNom() == nom) {
+	    		artReturned.add(art);
+	    	}	    	
+	    }
+		return artReturned;
+	}
+	
+	public ArrayList<Article> getArticlesLouesByPrix(float prixParJour) {
+		Iterator itr = articles.iterator();
+	    ArrayList<Article> artReturned = new ArrayList<Article>();
+	    while(itr.hasNext()) {
+	    	Article art = (Article) itr.next();
+	    	if(art.getPrixLocationParJour() == prixParJour) {
+	    		artReturned.add(art);
+	    	}	    	
+	    }
+		return artReturned;
+	}
 }
