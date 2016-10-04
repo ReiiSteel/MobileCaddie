c4701cf698e1de023e93615ae8c9069f996ab67f
William FLAMBARD <E145853L@U-INF-J-E114-10.iut-nantes.univ-nantes.prive>
2016-10-04 09:04:52 +0200

    ajout methode Location

--- srcEclipse/Location.java
+++ srcEclipse/Location.java
@@ -1,14 +1,78 @@
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class Location {
-	private Calendar dateDebut;
-	private Calendar dateFin;
+	private GregorianCalendar dateDebut;
+	private GregorianCalendar dateFin;
 	private ArrayList<Article> articles;
 	private Client client;
 	
-	public Location(Client client, ArrayList<Article> articles) {
-		Calendar cal = Calendar.getInstance();
-		this.dateDebut = cal.set(year, month, date);;
+	/**
+	 * Constructeur de la class Location
+	 * @param client : client qui fait la location
+	 * @param articles : articles loues par le client
+	 * @param year
+	 * @param month
+	 * @param day
+	 */
+	public Location(Client client, ArrayList<Article> articles, int year, int month, int day) {
+		GregorianCalendar datedebut = new GregorianCalendar();
+		this.dateDebut = datedebut;
+		this.client = client;
+		this.articles.addAll(articles);
+		this.dateFin = (GregorianCalendar) this.dateDebut.clone();
+		this.dateFin.add(Calendar.YEAR, year);
+		this.dateFin.add(Calendar.MONTH, month);
+		this.dateFin.add(Calendar.DATE, day);
+	}
+	
+	/**
+	 * Donne le montant a facturer sur la période complete
+	 * @return : 
+	 */
+	public float getMontantAFacturer(){
+		float montant = 0.0f;
+		for ( Article article : this.articles) {
+			montant += article.getPrixLocationParJour()*(this.dateDebut.get(2));
+		}
+		return montant;
+	}
+	
+	
+	//public float getMontantPeriode()
+	
+	//end loc retire client
+
+	public GregorianCalendar getDateDebut() {
+		return dateDebut;
+	}
+
+	public void setDateDebut(GregorianCalendar dateDebut) {
+		this.dateDebut = dateDebut;
+	}
+
+	public GregorianCalendar getDateFin() {
+		return dateFin;
+	}
+
+	public void setDateFin(GregorianCalendar dateFin) {
+		this.dateFin = dateFin;
+	}
+
+	public ArrayList<Article> getArticles() {
+		return articles;
+	}
+
+	public void setArticles(ArrayList<Article> articles) {
+		this.articles = articles;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
 	}
 }
