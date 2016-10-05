a8877e58f8324044cae600c8460c4ab4cbfb85ef
William FLAMBARD <E145853L@U-INF-J-E114-10.iut-nantes.univ-nantes.prive>
2016-10-05 17:25:50 +0200

    Main clean

--- srcEclipse/main/Main.java
+++ srcEclipse/main/Main.java
@@ -15,8 +15,8 @@ public class Main {
 	
 	public static void main(String[] args) throws IOException {
 		Magasin m1 = new Magasin("yo");
-		Client c1 = new Client("Nom", "Nantes");
-		Objectif obj = new Objectif("Nom", "ref", "modele", 5, 1, "type obj", 99442, "1920*1080");
+		Client c1 = new Client("Enzo", "Nantes");
+		Objectif obj = new Objectif("Focus", "O1L2H-OP", "Canon", 5, 1, "type obj", 99442, "1920*1080");
 		Camera cam = new Camera("Nom", "ref2", "modele", 2.5f, 1, "type obj", 999999, "1920*1080");
 		ArrayList<Article> arts = new ArrayList<Article>();
 		arts.add(obj);
@@ -29,32 +29,6 @@ public class Main {
 			System.out.println(e.getMessage());
 		}
 		
-		
-		String fichier = "";
-		GregorianCalendar dateEnCours = new GregorianCalendar();
-		fichier += dateEnCours.get(Calendar.YEAR);
-		int month = dateEnCours.get(Calendar.MONTH);
-		fichier += month;
-		fichier += ".loc";
-
-		// Ouverture du flux
-		DataInputStream fluxEntreeBinaire;
-		try {
-			fluxEntreeBinaire = new DataInputStream(new FileInputStream(fichier));
-			
-			int nbArticles = fluxEntreeBinaire.readInt();
-			char nomClient = fluxEntreeBinaire.readChar();
-			System.out.println(nbArticles);
-			System.out.println(nomClient);
-			fluxEntreeBinaire.close();
-			
-		} catch (FileNotFoundException e) {
-			System.out.println("Pas d'archive trouvée !");
-		}
-
-		
-
-		// Fermeture du flux
 		
 	}
 }
