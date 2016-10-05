5d2109bd22cb09ec27f1fd2f57734bc8ceaae7d3
William FLAMBARD <E145853L@U-INF-J-E114-10.iut-nantes.univ-nantes.prive>
2016-10-05 16:56:24 +0200

    Modification Article
    Modification des noms du constructeur (aucun changement de structure)

--- srcEclipse/main/Article.java
+++ srcEclipse/main/Article.java
@@ -11,13 +11,12 @@ public class Article {
 	private float prixLocationParJour;
 	private int nbStock;
 	
-	public Article(String nom, String reference, String modele, float prixLocationParJour, int nbStock) {
-		// ! Référence unique TODO
+	public Article(String nom, String reference, String marque, float prixLocationParJour, int nbStock) {
 		this.nom = nom;
 		this.reference = reference;
-		this.marque = modele;
+		this.marque = marque;
 		this.prixLocationParJour = prixLocationParJour;
-		this.nbStock = nbStock;		
+		this.nbStock = nbStock;	
 	}
 
 	public String getReference() {
@@ -40,12 +39,14 @@ public class Article {
 		return nbStock;
 	}
 	
-	public void louer(){
+	public boolean louer(){
 		if(this.nbStock > 0){ 
-			this.nbStock -= 1; 
+			this.nbStock -= 1;
+			return true;
 		}
 		else{
 			System.out.println("L'article "+this.marque+" "+this.nom+" "+this.reference+" n'est plus disponible en stock et ne peut être loué.");
+			return false;
 		}
 	}
 	
